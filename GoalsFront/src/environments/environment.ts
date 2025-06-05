a2a8f7902b0307f4eb74a347881365dd7f2ada77
Yunus Emre Coşkun <emre27coskun@gmail.com>
2025-06-05 22:02:31 +0300

    deneme26

--- GoalsFront/src/environments/environment.ts
+++ GoalsFront/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts (Production için)
 export const environment = {
   production: true,
-  apiUrl: '/api/'    // DİKKAT: mutlaka sonuna slash koyun
+  apiUrl: 'http://52.166.217.16:5026/'    // DİKKAT: mutlaka sonuna slash koyun
 };
